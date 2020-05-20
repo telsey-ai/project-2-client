80b7b50cd48dd427b22393dcaac7aa4c7f9717d9
telsey-ai <thomaswelsey@gmail.com>
2020-05-20 09:18:00 -0400

    Updated deployed link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://git.heroku.com/radiant-refuge-43345.git',
+  production: 'https://radiant-refuge-43345.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
